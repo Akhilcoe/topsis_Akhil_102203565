60ed772b16b75d532c24c3264fbd71453dac08d8
Akhilcoe <goyalakhil06022004@gmail.com>
2025-01-19 00:32:59 +0530

    uploading this python package

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
-# setup.py
 from setuptools import setup, find_packages
+
+# Read README file with proper encoding
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="topsis_Akhil_102203565",
     version="1.0.0",
-    description="A Python package for topsis(Technique for Order Prefrence by Similarity to ideal solution) algorithm",
-    long_description=open("README.md").read(),
+    description="A Python package for TOPSIS (Technique for Order Preference by Similarity to Ideal Solution) algorithm",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     author="Akhil Goyal",
     author_email="goyalakhil06022004@gmail.com",
-    url="https://github.com/yourusername/graphUtils",
+    url="https://github.com/Akhilcoe/topsis_Akhil_102203565",  # Update this with your real repo link
     packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,3 +25,4 @@ setup(
     ],
     python_requires=">=3.6",
 )
+
